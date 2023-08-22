6c36565a2ca26c442cc6b37a19a5f7f62c13c839
Abad <abad@masterinfotech.com>
2023-08-22 13:14:48 +0530

    acl code commented for dep

--- src/auth/role/role.guard.ts
+++ src/auth/role/role.guard.ts
@@ -6,20 +6,20 @@ import { Reflector } from '@nestjs/core';
 export class RoleGuard implements CanActivate {
   constructor(private reflector: Reflector) { }
 
-  matchRoles(roles: string[], userRole: string) {
-    return roles.some((role) => role == userRole)
-  }
+  // matchRoles(roles: string[], userRole: string) {
+  //   return roles.some((role) => role == userRole)
+  // }
 
 
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    const roles = this.reflector.get<string[]>('roles', context.getHandler());
-    if(!roles){
-      return true;
-    }
-    const request = context.swithToHttp().getRequest();
-    const user = request.user;
-    return true;
-  }
+  // canActivate(
+  //   context: ExecutionContext,
+  // ): boolean | Promise<boolean> | Observable<boolean> {
+  //   const roles = this.reflector.get<string[]>('roles', context.getHandler());
+  //   if(!roles){
+  //     return true;
+  //   }
+  //   const request = context.swithToHttp().getRequest();
+  //   const user = request.user;
+  //   return true;
+  // }
 }
