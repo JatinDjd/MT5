bf2c9b2e0c2e484903fd67ce1271007a65396555
Abad <abad@masterinfotech.com>
2023-08-22 13:12:59 +0530

    main file updated

--- src/auth/role/role.guard.ts
+++ src/auth/role/role.guard.ts
@@ -1,11 +1,25 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { Reflector } from '@nestjs/core';
 
 @Injectable()
 export class RoleGuard implements CanActivate {
+  constructor(private reflector: Reflector) { }
+
+  matchRoles(roles: string[], userRole: string) {
+    return roles.some((role) => role == userRole)
+  }
+
+
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    const roles = this.reflector.get<string[]>('roles', context.getHandler());
+    if(!roles){
+      return true;
+    }
+    const request = context.swithToHttp().getRequest();
+    const user = request.user;
     return true;
   }
 }
