f78ca3921abbe7e53096241609439a4dc4eadfce
Abad <abad@masterinfotech.com>
2023-09-21 17:16:09 +0530

    feed data updated

--- src/feeds/feeds.service.ts
+++ src/feeds/feeds.service.ts
@@ -7,70 +7,70 @@ import { Feed } from './entities/feed.entity';
 export class FeedsService {
   feeds: Feed[] = [
     {
-      "symbol": "EURUSD",
+      "symbol": "EUR/USD",
       "ts": "1694076457697",
       "bid": 1.24705,
       "ask": 1.24709,
       "mid": 1.24707
     },
     {
-      "symbol": "USDCHF",
+      "symbol": "USD/JPY",
       "ts": "1694076457697",
       "bid": 1.24685,
       "ask": 1.24689,
       "mid": 1.24687
     },
     {
-      "symbol": "GBPUSD",
+      "symbol": "GBP/USD",
       "ts": "1694076457697",
       "bid": 1.24675,
       "ask": 1.24679,
       "mid": 1.24677
     },
     {
-      "symbol": "USDJPY",
+      "symbol": "AUD/USD",
       "ts": "1694076457697",
       "bid": 1.24715,
       "ask": 1.24719,
       "mid": 1.24717
     },
     {
-      "symbol": "USDRUB",
+      "symbol": "USD/CAD",
       "ts": "1694076457697",
       "bid": 73.24695,
       "ask": 73.24699,
       "mid": 73.24697
     },
     {
-      "symbol": "AUSUSD",
+      "symbol": "NZD/USD",
       "ts": "1694076457697",
       "bid": 0.74695,
       "ask": 0.74699,
       "mid": 0.74697
     },
     {
-      "symbol": "NZDUSD",
+      "symbol": "USD/CHF",
       "ts": "1694076457697",
       "bid": 0.64695,
       "ask": 0.74699,
       "mid": 0.74697
     },
     {
-      "symbol": "USDCAD",
+      "symbol": "EUR/GBP",
       "ts": "1694076457697",
       "bid": 1.34695,
       "ask": 1.34699,
       "mid": 1.34697
     },
     {
-      "symbol": "USDSEK",
+      "symbol": "EUR/JPY",
       "ts": "1694076457697",
       "bid": 8.24695,
       "ask": 8.24699,
       "mid": 8.24697
     },
     {
-      "symbol": "USDHKD",
+      "symbol": "GBP/JPY",
       "ts": "1694076457697",
       "bid": 7.74695,
       "ask": 7.74699,
